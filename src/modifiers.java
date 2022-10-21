ab7613f868c1e78dfde54f74ef39b362532dbccf
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2022-10-21 16:53:32 -0400

    modifiers class started

---
+++ src/modifiers.java
@@ -0,0 +1,22 @@
+public class modifiers {
+    //Access Modifiers - controls the access level
+    //Non-Access Modifiers - do not control access level, but provides other functionality
+
+    //Access Modifiers
+    //For classes, you can use either public or default:
+    //For attributes, methods and constructors, you can use public, private, default and protected
+
+    //Non-Access Modifiers
+    //For classes, you can use either final or abstract:
+    //For attributes and methods, you can use final, static, abstract, transient, synchronized and volatile
+
+    //Final
+    //If you don't want the ability to override existing attribute values, declare attributes as final:
+
+    //Static
+    //A static method means that it can be accessed without creating an object of the class, unlike public:
+
+    //Abstract
+    //An abstract method belongs to an abstract class, and it does not have a body. The body is provided by the subclass:
+
+}
